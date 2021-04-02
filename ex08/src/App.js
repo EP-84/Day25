4f1ff39021441a354bf0d31ec4f456042b2df9e7
EP-84 <edmir.prasovic@gmail.com>
2021-04-02 02:47:11 +0200

    v1

---
+++ ex08/src/App.js
@@ -0,0 +1,14 @@
+import PostRequestSetHeaders from './PostRequestSetHeaders';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <PostRequestSetHeaders />
+
+    </div>
+  );
+}
+
+export default App;
